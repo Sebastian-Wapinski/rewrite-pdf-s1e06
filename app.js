a970dfaaa0a7f1e3371affc4e367e609c8cfdaed
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-04-19 14:05:38 +0200

    Capturing remove

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const itemsList = document.querySelectorAll(
     'section, article, h1'
 )
 
-const showTagName = function () {
-    console.log(this.tagName.toLowerCase());
+const showTagName = function (e) {
+    console.log(this.tagName);
+    if (this.tagName === 'ARTICLE') {
+        this.removeEventListener(
+            'click', showTagName, true
+        )
+    }
 }
 
 itemsList.forEach(function (item) {
