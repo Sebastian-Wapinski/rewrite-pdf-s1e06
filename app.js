e84ef009b9e6bad3e38f757467d8b368dad5dcb5
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-04-19 14:49:03 +0200

    preventDefault

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-const btnsList = document.querySelectorAll('button')
-const pElement = document.querySelector('p')
+const linksList = document.querySelectorAll('a')
 
-const renderInfo = function (e) {
-    if (pElement) {
-        let text = parseInt(e.timeStamp / 1000)
-        text += ': ' + this.tagName;
-        text += ' -> ' + e.type;
-        pElement.innerText = text
-    }
+const getHref = function (event) {
+    event.preventDefault();
+
+    const href = this.getAttribute('href')
+    console.log(href);
 }
 
-btnsList.forEach(function (item) {
-    item.addEventListener('click', renderInfo)
-    item.addEventListener('mouseenter', renderInfo)
+linksList.forEach(function (item) {
+    item.addEventListener('click', getHref)
 })
+
+
 
 
